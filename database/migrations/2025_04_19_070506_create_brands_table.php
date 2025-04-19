96af2240b4d686b11f15f4f440e05cba07aa902c
Nkengbeza Clinton <nkengbeza@yandex.ru>
2025-04-19 09:20:27 +0100

    feat(#1): set up brand schema

---
+++ database/migrations/2025_04_19_070506_create_brands_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique('uk_brands_name')->nullable(false);
+            $table->string('image')->nullable(false);
+            $table->unsignedTinyInteger('rating')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
